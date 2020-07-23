171dcdd8d852953008baaef4eb0dae6a2ea5c77d
Krzysztof Szot <krzysztof.szot@PL-MAC-C02YT1Z3LVDM.local>
2020-07-23 09:06:19 +0200

    Fix css chunking, remove deprecated packages

--- next.config.js
+++ next.config.js
@@ -1,17 +1,4 @@
-const withSass = require('@zeit/next-sass')
-const sass = require('sass')
-
 module.exports = {
-  ...withSass({
-    cssModules: true,
-    sassLoaderOptions: {
-      implementation: sass,
-    },
-    cssLoaderOptions: {
-      importLoaders: 1,
-      localIdentName: "[local]___[hash:base64:5]",
-    },
-  }),
   env: {
     useMockApi: false,
     apiBaseUrl: 'http://localhost:4000/',
