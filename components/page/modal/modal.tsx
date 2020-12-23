128c7dcaff7d1ab35a9c0a5f5a30e393fd079bc6
Krzysztof Szot <krzysztof.szot@cognifide.com>
2020-12-23 11:27:52 +0100

    modal persistence option

--- components/page/modal/modal.tsx
+++ components/page/modal/modal.tsx
@@ -1,17 +1,41 @@
 import styles from './modal.module.scss'
 import classNames from 'classnames'
+import { useCallback, useEffect, useRef } from 'react'
 
 type Props = {
-  open?: boolean
+  open: boolean
+  onClose: () => void
   children: React.ReactNode
   width?: number
   height?: number
+  persistent?: boolean
 }
 
 export const Modal: React.FunctionComponent<Props> = (props: Props) => {
+  const overlayRef = useRef<HTMLDivElement>(null)
+  const listenerRef = useRef<boolean>(false)
+  const persistenceCallback = useCallback((e: MouseEvent) => {
+    if (overlayRef.current === e.target) {
+      props.onClose()
+    }
+  }, [])
+
+  useEffect(() => {
+    if (props.open && !props.persistent) {
+      window.addEventListener('click', persistenceCallback)
+      listenerRef.current = true
+
+      return () => {
+        window.removeEventListener('click', persistenceCallback)
+      }
+    } else if (listenerRef.current) {
+      window.removeEventListener('click', persistenceCallback)
+    }
+  }, [props.open, props.persistent])
+
   return (
     <>
-      <div className={classNames(styles.overlay, { [styles['is-visible']]: props.open })}></div>
+      <div ref={overlayRef} className={classNames(styles.overlay, { [styles['is-visible']]: props.open })}></div>
       <dialog
         style={{
           width: props.width ? props.width : 'max-content',
